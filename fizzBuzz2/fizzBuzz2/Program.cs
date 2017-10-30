d88df85341fc7bdb4f3846c7328b3dec34514fe9
TrentNagy <32527200+TrentLNagy@users.noreply.github.com>
2017-10-30 09:27:54 -0400

    Update Program.cs
    Added description and more in-depth explanation

--- fizzBuzz2/fizzBuzz2/Program.cs
+++ fizzBuzz2/fizzBuzz2/Program.cs
@@ -1,4 +1,24 @@
-﻿using System;
+/* This is my answer to the Fizz Buzz problem. This program is designed to print 
+out numbers 1 - 100 with each number having its own line. Every time the number
+is divisible by 3, then it will replace the number with the word "Fizz". If the 
+number is divisible by 5, then it will replace the number with "Buzz". If both apply,
+then replace the number with "FizzBuzz". Otherwise, if they aren't divisible, then it
+will show the normal number.
+
+  I've designed this program to be a little more adaptable than other fizzbuzz solutions,
+  allbeit a little more complex than it probably should be. I've declared both fizz and buzz
+  as changeable variables, so the user can output whatever they want instead of fizz/buzz.
+  I've also declared 3 variables for the factors of the division: The fizz division, the buzz 
+  division, and the desired remainder. All of them are set to the defaults.
+  
+    Now, I understand people will look at this and say that it's more complicated than it needs
+    to be, and "I can write it in only one line". But I'm not going for simple and fast, im planning
+    more ahead when it comes to changing ideas. Soon, when i become a better programer, i'll put this 
+    through Visual Studio and you will be able to add or remove different fizz/buzz variables.
+      
+                                                                          -Trent Nagy
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
